680a718dea14029b55da1c1ee6c30d27877260b9
CheshtaK <32400008+CheshtaK@users.noreply.github.com>
2018-06-07 10:30:41 +0530

    removing stop words from text

---
+++ StopWords.py
@@ -0,0 +1,17 @@
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+text = "This is a sample sentence, showing off the stop words filtration."
+
+stop_words = set(stopwords.words('english'))
+word_tokens = word_tokenize(text)
+
+filtered_sentence = [w for w in word_tokens if w not in stop_words]
+filtered_sentence = []
+
+for w in word_tokens:
+    if w not in stop_words:
+        filtered_sentence.append(w)
+
+print(word_tokens)
+print(filtered_sentence)
